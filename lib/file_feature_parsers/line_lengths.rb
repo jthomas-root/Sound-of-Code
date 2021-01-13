e8567c1f97c345712559d91ac8c37c9e4f546a71
Josh Thomas <josh.thomas@joinroot.com>
2021-01-12 20:53:16 -0500

    Fix line_lengths parser

--- lib/file_feature_parsers/line_lengths.rb
+++ lib/file_feature_parsers/line_lengths.rb
@@ -1,14 +1,14 @@
 module LineLengths
   def self.parse_line(line)
-    line.length - 1
+    line.nil? ? 0 : line.length - 1
   end
 
   def self.parse_file(lines)
     lengths = lines.map(&:length)
 
     OpenStruct.new(
-      :max => lengths.max - 1,
-      :min => lengths.min - 1,
+      :max => (lengths.max || 1) - 1,
+      :min => (lengths.min || 1) - 1,
     )
   end
 end
